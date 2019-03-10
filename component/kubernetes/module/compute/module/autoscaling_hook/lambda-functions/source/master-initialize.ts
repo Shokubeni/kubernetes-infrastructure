2897a6115066c0c7e9617883af12de5ac5736278
Ruslan Safin <r.safin89@gmail.com>
2019-03-10 18:38:54 +0300

    Nodes initialization speed up

--- component/kubernetes/module/compute/module/autoscaling_hook/lambda-functions/source/master-initialize.ts
+++ component/kubernetes/module/compute/module/autoscaling_hook/lambda-functions/source/master-initialize.ts
@@ -4,7 +4,6 @@ import { completeLifecycle, isMasterNodeExists, refreshLifecycle, setInstanceTag
 import { LifecycleEvent, LifecycleResult, NodeRole, NodeState, TagName } from './helper/autoscaling/types';
 import { deleteQueueTask, refreshQueueTask } from './helper/queue';
 import { runCommand, isInSystemManager } from './helper/manager';
-import { TASK_REFRESH_TIMEOUT } from './constant/timeouts';
 import { HandlerMessages } from './constant/messages';
 
 declare var process : {
@@ -13,8 +12,10 @@ declare var process : {
     GENERAL_MASTER_INIT_COMMAND: string,
     STACKED_MASTER_INIT_COMMAND: string,
     MASTER_AUTOSCALING_GROUP: string,
-    LOAD_BALANCER_DNS: string
+    TASK_REFRESH_TIMEOUT: string,
+    TASK_EXECUTE_LIMIT: string,
     KUBERNETES_VERSION: string,
+    LOAD_BALANCER_DNS: string,
     DOCKER_VERSION: string,
     S3_BUCKED_NAME: string,
     SQS_QUEUE_URL: string,
@@ -23,43 +24,55 @@ declare var process : {
 };
 
 export const handler = async (event: SQSEvent, context: Context): Promise<void> => {
-  const { Event } = JSON.parse(event.Records[0].body);
+  try {
+    const { Event } = JSON.parse(event.Records[0].body);
 
-  if (Event === LifecycleEvent.TestNotification) {
+    if (Event === LifecycleEvent.TestNotification) {
+      await deleteQueueTask(event, process.env.SQS_QUEUE_URL);
+      return context.succeed(HandlerMessages.TestNotificationHandled);
+    }
+
+    const refreshTimeout = parseInt(process.env.TASK_REFRESH_TIMEOUT, 10);
+    const executeLimit = parseInt(process.env.TASK_EXECUTE_LIMIT, 10);
+
+    if (!await isInSystemManager(event)) {
+      await refreshQueueTask(event, process.env.SQS_QUEUE_URL, refreshTimeout);
+      return context.fail(HandlerMessages.TaskHandlingDelayed);
+    }
+
+    await refreshQueueTask(event, process.env.SQS_QUEUE_URL, executeLimit);
+    await runCommand(event, process.env.NODE_RUNTIME_INSTALL_COMMAND, {
+      KubernetesVersion: [process.env.KUBERNETES_VERSION],
+      DockerVersion: [process.env.DOCKER_VERSION],
+    });
+
+    if (await isMasterNodeExists(process.env.MASTER_AUTOSCALING_GROUP)) {
+      await refreshLifecycle(event);
+      await runCommand(event, process.env.STACKED_MASTER_INIT_COMMAND, {
+        S3BucketName: [process.env.S3_BUCKED_NAME],
+      });
+      await completeLifecycle(event, LifecycleResult.Continue);
+
+    } else {
+      await completeLifecycle(event, LifecycleResult.Continue);
+      await runCommand(event, process.env.GENERAL_MASTER_INIT_COMMAND, {
+        S3BucketName: [process.env.S3_BUCKED_NAME],
+        BalancerDNS: [process.env.LOAD_BALANCER_DNS],
+        ClusterId: [process.env.CLUSTER_ID],
+      });
+    }
+
+    await setInstanceTags(event, [
+      { Key: TagName.NodeState, Value: NodeState.NodeInitialized },
+      { Key: TagName.NodeRole, Value: NodeRole.MaterNode },
+    ]);
+
     await deleteQueueTask(event, process.env.SQS_QUEUE_URL);
-    return context.succeed(HandlerMessages.TestNotificationHandled);
+    return context.succeed(HandlerMessages.MasterNodeInitialized);
+
+  } catch (exception) {
+    await deleteQueueTask(event, process.env.SQS_QUEUE_URL);
+    await completeLifecycle(event, LifecycleResult.Abandon);
+    throw exception;
   }
-
-  if (!await isInSystemManager(event)) {
-    await refreshQueueTask(event, process.env.SQS_QUEUE_URL, TASK_REFRESH_TIMEOUT);
-    return context.fail(HandlerMessages.TaskHandlingDelayed);
-  }
-
-  await deleteQueueTask(event, process.env.SQS_QUEUE_URL);
-  await runCommand(event, process.env.NODE_RUNTIME_INSTALL_COMMAND, {
-    KubernetesVersion: [process.env.KUBERNETES_VERSION],
-    DockerVersion: [process.env.DOCKER_VERSION],
-  });
-
-  if (await isMasterNodeExists(process.env.MASTER_AUTOSCALING_GROUP)) {
-    await refreshLifecycle(event);
-    await runCommand(event, process.env.STACKED_MASTER_INIT_COMMAND, {
-      S3BucketName: [process.env.S3_BUCKED_NAME],
-    });
-    await completeLifecycle(event, LifecycleResult.Continue);
-
-  } else {
-    await completeLifecycle(event, LifecycleResult.Continue);
-    await runCommand(event, process.env.GENERAL_MASTER_INIT_COMMAND, {
-      S3BucketName: [process.env.S3_BUCKED_NAME],
-      BalancerDNS: [process.env.LOAD_BALANCER_DNS],
-      ClusterId: [process.env.CLUSTER_ID],
-    });
-  }
-
-  await setInstanceTags(event, [
-    { Key: TagName.NodeState, Value: NodeState.NodeInitialized },
-    { Key: TagName.NodeRole, Value: NodeRole.MaterNode },
-  ]);
-  return context.succeed(HandlerMessages.MasterNodeInitialized);
 };
--- component/kubernetes/module/compute/module/autoscaling_hook/lambda-functions/source/worker-initialize.ts
+++ component/kubernetes/module/compute/module/autoscaling_hook/lambda-functions/source/worker-initialize.ts
@@ -4,7 +4,6 @@ import { completeLifecycle, isMasterNodeExists, refreshLifecycle, setInstanceTag
 import { LifecycleEvent, LifecycleResult, NodeRole, NodeState, TagName } from './helper/autoscaling/types';
 import { deleteQueueTask, refreshQueueTask } from './helper/queue';
 import { runCommand, isInSystemManager } from './helper/manager';
-import { TASK_REFRESH_TIMEOUT } from './constant/timeouts';
 import { HandlerMessages } from './constant/messages';
 
 declare var process : {
@@ -12,6 +11,8 @@ declare var process : {
     NODE_RUNTIME_INSTALL_COMMAND: string,
     COMMON_WORKER_INIT_COMMAND: string,
     MASTER_AUTOSCALING_GROUP: string,
+    TASK_REFRESH_TIMEOUT: string,
+    TASK_EXECUTE_LIMIT: string,
     KUBERNETES_VERSION: string,
     DOCKER_VERSION: string,
     S3_BUCKED_NAME: string,
@@ -21,38 +22,46 @@ declare var process : {
 };
 
 export const handler = async (event: SQSEvent, context: Context): Promise<void> => {
-  const { Event } = JSON.parse(event.Records[0].body);
+  try {
+    const { Event } = JSON.parse(event.Records[0].body);
 
-  if (Event === LifecycleEvent.TestNotification) {
-    await deleteQueueTask(event, process.env.SQS_QUEUE_URL);
-    return context.succeed(HandlerMessages.TestNotificationHandled);
-  }
+    if (Event === LifecycleEvent.TestNotification) {
+      await deleteQueueTask(event, process.env.SQS_QUEUE_URL);
+      return context.succeed(HandlerMessages.TestNotificationHandled);
+    }
 
-  if (!await isInSystemManager(event)) {
-    await refreshQueueTask(event, process.env.SQS_QUEUE_URL, TASK_REFRESH_TIMEOUT);
+    const refreshTimeout = parseInt(process.env.TASK_REFRESH_TIMEOUT, 10);
+    if (!await isInSystemManager(event)) {
+      await refreshQueueTask(event, process.env.SQS_QUEUE_URL, refreshTimeout);
+      return context.fail(HandlerMessages.TaskHandlingDelayed);
+    }
+
+    if (await isMasterNodeExists(process.env.MASTER_AUTOSCALING_GROUP)) {
+      await refreshLifecycle(event);
+      await runCommand(event, process.env.NODE_RUNTIME_INSTALL_COMMAND, {
+        KubernetesVersion: [process.env.KUBERNETES_VERSION],
+        DockerVersion: [process.env.DOCKER_VERSION],
+      });
+
+      await runCommand(event, process.env.COMMON_WORKER_INIT_COMMAND, {
+        S3BucketName: [process.env.S3_BUCKED_NAME],
+      });
+      await setInstanceTags(event, [
+        { Key: TagName.NodeState, Value: NodeState.NodeInitialized },
+        { Key: TagName.NodeRole, Value: NodeRole.WorkerNode },
+      ]);
+
+      await completeLifecycle(event, LifecycleResult.Continue);
+      await deleteQueueTask(event, process.env.SQS_QUEUE_URL);
+      return context.succeed(HandlerMessages.WorkerNodeInitialized);
+    }
+
+    await refreshQueueTask(event, process.env.SQS_QUEUE_URL, refreshTimeout);
     return context.fail(HandlerMessages.TaskHandlingDelayed);
-  }
 
-  if (await isMasterNodeExists(process.env.MASTER_AUTOSCALING_GROUP)) {
-    await refreshLifecycle(event);
-    await runCommand(event, process.env.NODE_RUNTIME_INSTALL_COMMAND, {
-      KubernetesVersion: [process.env.KUBERNETES_VERSION],
-      DockerVersion: [process.env.DOCKER_VERSION],
-    });
-
-    await runCommand(event, process.env.COMMON_WORKER_INIT_COMMAND, {
-      S3BucketName: [process.env.S3_BUCKED_NAME],
-    });
-    await setInstanceTags(event, [
-      { Key: TagName.NodeState, Value: NodeState.NodeInitialized },
-      { Key: TagName.NodeRole, Value: NodeRole.WorkerNode },
-    ]);
-
-    await completeLifecycle(event, LifecycleResult.Continue);
+  } catch (exception) {
     await deleteQueueTask(event, process.env.SQS_QUEUE_URL);
-    return context.succeed(HandlerMessages.WorkerNodeInitialized);
+    await completeLifecycle(event, LifecycleResult.Abandon);
+    throw exception;
   }
-
-  await refreshQueueTask(event, process.env.SQS_QUEUE_URL, TASK_REFRESH_TIMEOUT);
-  return context.fail(HandlerMessages.TaskHandlingDelayed);
 };
