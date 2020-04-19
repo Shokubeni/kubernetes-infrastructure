deefaf301edf559ce85b8fdb631067b7e9f3164a
Ruslan Safin <r.safin89@gmail.com>
2020-04-20 00:29:32 +0300

    Dns fix

--- component/basic-deployments/module/dns-configuration/main.tf
+++ component/basic-deployments/module/dns-configuration/main.tf
@@ -16,9 +16,21 @@ resource "aws_route53_record" "metrics" {
   }
 }
 
-resource "aws_route53_record" "general" {
+resource "aws_route53_record" "public" {
   name    = var.network_config.domain_info.domain_name
   zone_id = var.network_config.domain_info.public_zone
+  type    = "A"
+
+  alias {
+    evaluate_target_health = false
+    zone_id = var.balancer_data.zone
+    name    = var.balancer_data.dns
+  }
+}
+
+resource "aws_route53_record" "private" {
+  name    = var.network_config.domain_info.domain_name
+  zone_id = var.network_config.domain_info.private_zone
   type    = "A"
 
   alias {
