0d4a7276d95c29fad7880468b4f6abc27c83f5bc
ruslan.safin <ruslan.safin@metal-city.ru>
2021-07-11 20:07:35 +0300

    www domain added to DNS configuration

--- component/basic-deployments/module/dns-configuration/main.tf
+++ component/basic-deployments/module/dns-configuration/main.tf
@@ -36,6 +36,18 @@ resource "aws_route53_record" "vpn" {
   }
 }
 
+resource "aws_route53_record" "www_kaliningrad" {
+  name    = "www.kaliningrad.${var.network_config.domain_info.domain_name}"
+  zone_id = var.network_config.domain_info.public_zone
+  type    = "A"
+
+  alias {
+    evaluate_target_health = false
+    zone_id = data.aws_elb_hosted_zone_id.main.id
+    name    = var.balancer_hostname
+  }
+}
+
 resource "aws_route53_record" "kaliningrad" {
   name    = "kaliningrad.${var.network_config.domain_info.domain_name}"
   zone_id = var.network_config.domain_info.public_zone
